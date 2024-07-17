8ecdafce85ecc7868143ae700ff8f080f450dbd5
UJJAWAL RANA <105837804+Ranaujjawal@users.noreply.github.com>
2024-07-17 23:24:02 +0530

    Update leetcodeinfo.js
    added code chef info also

--- leetcodeinfo.js
+++ leetcodeinfo.js
@@ -1,3 +1,6 @@
+const axios = require("axios");
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 const query=`
 query getUserProfile($username: String!) {
  allQuestionsCount {
@@ -68,4 +71,26 @@ const formatData = (data) => {
         console.error('Error', err);
         res.send(err);
     });
+}
+
+exports.chefinfo= async(req,res)=>{
+  try{
+  let data = await axios.get(`https://www.codechef.com/users/${req.params.id}`);
+  let dom = new JSDOM(data.data);
+  let document = dom.window.document;
+        res.status(200).send({
+            success: true,
+            profile: document.querySelector('.user-details-container').children[0].children[0].src,
+            name: document.querySelector('.user-details-container').children[0].children[1].textContent,
+            currentRating: parseInt(document.querySelector(".rating-number").textContent),
+            highestRating: parseInt(document.querySelector(".rating-number").parentNode.children[4].textContent.split('Rating')[1]),
+            countryName: document.querySelector('.user-country-name').textContent,
+            globalRank: parseInt(document.querySelector('.rating-ranks').children[0].children[0].children[0].children[0].innerHTML),
+            countryRank: parseInt(document.querySelector('.rating-ranks').children[0].children[1].children[0].children[0].innerHTML),
+            stars: document.querySelector('.rating').textContent || "unrated",
+        });
+  }
+  catch(err){
+    res.send({ success: false, error: err });
+  }
 }
