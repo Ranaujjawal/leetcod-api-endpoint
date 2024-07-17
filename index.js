b7f2ca694ad6f7a1bf2dfafc7b6cfb6a3fe50777
UJJAWAL RANA <105837804+Ranaujjawal@users.noreply.github.com>
2024-07-17 23:25:08 +0530

    Update index.js
    make seprate routes

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ app.use(Cors({
     origin:'*'
 }))
 
-
-app.get('/:id',Leet.info);
+app.get('/',(req,res)=>{
+  res.send('<br>write leet for leetcode at the end of url and then user-name or chef for code chef and then user-name </br>')
+})
+app.get('/leet/:id',Leet.info);
+app.get('/chef/:id',Leet.chefinfo);
   const port=5000;
   const server = app.listen(port);
